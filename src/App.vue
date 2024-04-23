68b2860bc2763189208a7c2bd244d2d33f2f8d39
ericNKS <ek.silva.santos@gmail.com>
2024-04-23 11:49:17 -0300

    Melhorando a seção de experiência para ficar visualmente mais atraente

--- src/App.vue
+++ src/App.vue
@@ -27,9 +27,24 @@ import CurriculoContato from './components/CurriculoContato.vue';
   data(){
     return{
       curriculo:{
-        name: "Caio Oliveira Cardoso",
+        name: "Éric Silva dos Santos",
         aboutMe: "Sou um estudante de Ciência da Computação, entusiasta em tecnologia desde criança, que nessa época não aguentava ver algo que se mexia e já queria abrir para ver tirar a minha duvida de como aquilo funcionava. Amo passar o tempo programando projetos pessoais como esse, amor jogar, assistir bons filmes como Clube da luta e Gênio Indomável, além de assistir animes e principalmente estar com a minha noiva.",
-        objetivo: "Tenho como objetivo de vida trazer inovação para o mundo usando a tecnologia como aliada. Gostaria de junto com a programação solucionar problemas reais, que ajudem pessoas reais e melhore a vida dessas pessoas. Pretendo fazer isso de formas criativas e aplicando uma solução de forma rápida porem competente, para que assim a solução seja realmente usual e não somente mais uma aplicação feita que acaba em desuso."
+        objetivo: "Tenho como objetivo de vida trazer inovação para o mundo usando a tecnologia como aliada. Gostaria de junto com a programação solucionar problemas reais, que ajudem pessoas reais e melhore a vida dessas pessoas. Pretendo fazer isso de formas criativas e aplicando uma solução de forma rápida porem competente, para que assim a solução seja realmente usual e não somente mais uma aplicação feita que acaba em desuso.",
+        experiencias: [
+          {
+            empresa: 'Politimax',
+            cargo: 'Suporte de TI',
+            dataInicio: '2024',
+            descricao: 'Auxilio os clientes para uma melhor utilização do software, tiro dúvidas que eles tiverem, incentivo a usar feactures que eles não estão utilizando com tanta frequência, alem de ajudar a desenvolver novas ferramentas utilizando PHP com zend framework, MySQL, javascript, jquery, ajax e bootstrap 4.'
+          },
+          {
+          empresa: 'Freelancer',
+          cargo: 'Software Engineer',
+          dataInicio: '2023',
+          dataFinal: '2023',
+          descricao: 'Fiz uma aplicação web de realizar check-in para uma empresa que tem seus funcionários em estilo home office para poder ter o controle de entrada e saída deles do trabalho. Para isso eu utilizei PHP, MySQL, javascript, ajax e Bootstrap.'
+        }
+        ],
       }
     }
   }
--- src/components/CurriculoContato.vue
+++ src/components/CurriculoContato.vue
@@ -1,5 +1,63 @@
 <template>
     <div class="container">
+
+        <div class="row">
+            <div class="col-12">
+                
+                <div class="row mb-2">
+                    <div class="col-12">
+                        <h3>Experiências</h3>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-2">
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                    </div>
+                    <div class="col-8">
+                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+
+                            <div class="carousel-inner">
+
+                                
+                                <div  v-for="(item, index) in curriculo.experiencias" :key="item" v-bind:class="index == 0 ? 'carousel-item active' : 'carousel-item'" >
+                            
+                                    <div class="card bg-dark">
+                                        <div class="card-header">
+                                            <h5 class="card-title"> {{ item.empresa }} - {{ item.cargo }} </h5>
+                                            <small> {{ item.dataInicio }} - {{ item.dataFinal ? item.dataFinal : 'Atual' }} </small>
+                                        </div>
+                                        <div class="card-body">
+                                            <p class="card-text">
+                                                {{ item.descricao }}
+                                                <!-- Auxilio os clientes para uma melhor utilização do software, tiro dúvidas que eles tiverem, incentivo a usar feactures que eles não estão utilizando com tanta frequência,
+                                                alem de ajudar a desenvolver novas ferramentas utilizando PHP com zend framework, javascript, ajax e bootstrap 4. -->
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
+                        
+                    </div>
+
+                    <div class="col-2">
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                    
+                </div>
+                
+            </div>
+        </div>
+
         <div class="row">
             <div class="col-sm-12 col-md-6 order-md-1 order-2 contato">
 
@@ -30,7 +88,7 @@
 
             </div>
 
-            <!-- Objetivos e experiencias -->
+            <!-- Objetivos -->
             <div class="col-sm-12 col-md-6 order-md-2 order-1 contato">
 
                 <div class="row">
@@ -43,20 +101,9 @@
                         </div>
                     </div>
                 </div>
-
-                <div class="row">
-                    <div class="col-12">
-                        <h3>Experiências</h3>
-                        <div>
-                            <ul>
-                                <li>teste</li>
-                                <li>teste 2</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
+        
     </div>
 </template>
 
@@ -72,20 +119,21 @@ export default defineComponent({
 
 
 <style scoped>
+.card{
+    margin: auto;
+    max-width: 850px;
+}
 a{
     color: #aaaaaa;
 }
 .contato{
     display: flex;
-    justify-content: center;
-    text-align: center;
     flex-flow: column;
-    height: 50vh;
+    margin-top: 4em;
 }
 .contato h3{
     font-size: 1.5em;
     font-weight: bold;
-    text-align: center;
 }
 .container{
     font-size: 0.77rem;
